5f93c0aa2d6c125c51d87f5eadccc83ae7dd5d1e
Kamil <kmlkalicki@gmail.com>
2016-04-21 19:26:02 +0200

    Zmiana preloadera i ustawienie particles dla hero

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
     $('#preloader-circle').css('transform', 'scale(0)');
     
     setTimeout(function(){
-        $('.preloader').addClass('preloader-hide');}, 500);
+        $('.preloader').addClass('preloader-hide');
+        $('#preloader-left').css('left', '-100%'); 
+        $('#preloader-right').css('border-width', '0');   
+    }, 500);
+    
+    setTimeout(function(){
+        $('#preloader-left, #preloader-right').hide();
+    },1300);
+    
     
 });
