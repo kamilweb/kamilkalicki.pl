7697f06af26a89f4e7b73f58d13e2d7a83825e94
Kamil <kmlkalicki@gmail.com>
2016-04-25 19:26:23 +0200

    Dodanie wszystkich funkcjonalności do hero

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
     
+    var heroHeight = $('#hero').outerHeight();
+    
+     $(document).scroll(function(){
+        if ($('#hero').scrollTop() >= 100) {
+            $('body').animate({scrollTop: heroHeight}, 1000);
+        }
+    });
+    
+    $('.inactive-a').click(function(){
+        event.preventDefault();
+    });
+    
+    $('#arrow-down, #check').click(function(){
+        $('body').animate({ scrollTop: heroHeight}, 1000);
+    });
+    
     // Preloader
     $('#preloader-circle').css('transform', 'scale(0)');
     
