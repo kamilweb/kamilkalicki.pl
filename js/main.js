b8f7e6aa3acab2e6c27decb8d72dec9102e4e3f0
Kamil <kmlkalicki@gmail.com>
2016-04-26 21:58:09 +0200

    Wypozycjonowanie górnej sekcji about-me i pierwsze ustawienia dla dolnej sekcji

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
 $(document).ready(function(){
     
-    var heroHeight = $('#hero').outerHeight();
+     // Preloader
+    $('#preloader-circle').css('transform', 'scale(0)');
     
-     $(document).scroll(function(){
-        if ($('#hero').scrollTop() >= 100) {
-            $('body').animate({scrollTop: heroHeight}, 1000);
-        }
-    });
+    $('#logo-box').css('top', '50%');
+    
+    setTimeout(function(){
+        $('.logo-left').addClass('logo-left-up');
+        $('.logo-right').addClass('logo-right-down');
+    }, 500);
+    
+    setTimeout(function(){
+        $('.preloader').addClass('preloader-hide');
+        $('#preloader-left, #preloader-right').css('border-width', '0');
+        $('#logo-box').hide();
+    }, 1000);
+    
+    
+    // Main
     
     $('.inactive-a').click(function(){
         event.preventDefault();
     });
     
     $('#arrow-down, #check-out').click(function(){
-        $('body').animate({ scrollTop: heroHeight}, 1000);
+        $('body').animate({ scrollTop: $('#about-me').offset().top + 1}, 1000);
     });
-    
-    // Preloader
-    $('#preloader-circle').css('transform', 'scale(0)');
-    
-    setTimeout(function(){
-        $('.preloader').addClass('preloader-hide');
-        $('#preloader-left, #preloader-right').css('border-width', '0');   
-    }, 500);
 });
