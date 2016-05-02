d329368c43db19d365ed401911db692391d37411
Kamil <kmlkalicki@gmail.com>
2016-05-02 21:30:30 +0200

    Poprawki w górnej części strony, dodanie efektu parallax dla hero i about-me, walidacja formularza

--- js/main.js
+++ js/main.js
@@ -1,52 +1,70 @@
 $(document).ready(function(){
     
-     // Preloader
-    $('#preloader-circle').css('transform', 'scale(0)');
-    
-    $('#logo-box').css('top', '50%');
-    
+// Preloader
     setTimeout(function(){
-        $('.logo-left').addClass('logo-left-up');
-        $('.logo-right').addClass('logo-right-down');
-    }, 500);
+        
+        $('#logo-box').css('top', '50%');
     
-    setTimeout(function(){
-        $('#preloader-left, #preloader-right').addClass('preloader-hide');
-        $('#logo-box').hide();
-    }, 1000);
+        $('#preloader-circle').css('transform', 'scale(0)');
+
+        setTimeout(function(){
+            $('.logo-left').addClass('logo-left-up');
+            $('.logo-right').addClass('logo-right-down');
+        }, 500);
+
+        setTimeout(function(){
+            $('#logo-box').hide();
+            $('#preloader-left, #preloader-right').animate({borderRightWidth: 0, borderLeftWidth: 0},800);
+        }, 1000);
+    },300);
     
     
-    // Main
-    
-    
-    
-    $('i#msg-icon').click(function(){
-        $('html, body').animate({ scrollTop: $('#lets-work').offset().top}, 1000);
-    });
-    
+// Main
     $('#arrow-down, #check-out').click(function(){
         $('html, body').animate({ scrollTop: $('#about-me').offset().top + 1}, 1500);
     });
     
-    // Owl carousel
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        autoplay: true,
-        autoplayTime: 3000,
-        margin: 20,
-        autoWidth: true,
-        center: 1,
-        dots: false,
-        nav: false
+// Skrollr 
+    if($(window).width() >= 768){
+        var s = skrollr.init();
+    }
+    
+// Let's work together
+    
+    $('#msg-form').submit(function(){
+        
+        var redBtn = $('#msg-form .blue-btn').addClass('red-btn');
+        
+        if($('#email').val() == ""){
+            $('#email').addClass('red-line');
+            redBtn
+        }
+        if($('#name').val() == ""){
+            $('#name').addClass('red-line');
+            redBtn
+        }
+        if($('#phone').val() == ""){
+            $('#phone').addClass('red-line');
+            redBtn
+        }
+        if($('#topic').val() == ""){
+            $('#topic').addClass('red-line');
+            redBtn
+        }
+        if($('textarea').val() == ""){
+            $('textarea').addClass('red-line');
+            redBtn
+        }
+        if(($('textarea').val() == "") || ($('#name').val() == "") ||                   ($('#topic').val() == "") || ($('#phone').val() == "") ||                   ($('#email').val() == "") ) {
+            return false
+        };   
     });
     
-    // Let's work together
-    $('#msg-form').submit(function(){
-        if(($('#email').val() == "") || ($('textarea').val() == "") || ($('#name').val() == "") || ($('#phone').val() == "") || ($('#topic').val() == ""))
-        {
-            alert("Uzupełnij wymagane pola");
-            return false;
-        }
+
+    $('#lets-work').click(function(){
+        $('.msg-input, textarea').removeClass('red-line');
+        $('#msg-form .blue-btn').removeClass('red-btn');
     });
+    
     
 });
